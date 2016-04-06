9b302491b743a55f6ca70e6f9d8e13eaba6442a4
catpoehlman <cat.poehlman@gmail.com>
2016-04-06 11:21:39 -0400

    formatting changes

--- src/ca/sheridancollege/beans/Builder.java
+++ src/ca/sheridancollege/beans/Builder.java
@@ -3,7 +3,7 @@ package ca.sheridancollege.beans;
 import java.io.Serializable;
 
 public class Builder implements Serializable {
-	
+
 	private String vendorName;
 	private String vendorAddress;
 	private long vendorRefNum;
@@ -11,11 +11,11 @@ public class Builder implements Serializable {
 	private long builderRefNum;
 	private String builderAddress;
 	private int builderPhoneNumber;
-	
-	public Builder(){
-		
+
+	public Builder() {
+
 	}
-	
+
 	public Builder(String vendorName, String vendorAddress, long vendorRefNum, String builderName, long builderRefNum,
 			String builderAddress, int builderPhoneNumber) {
 		this.vendorName = vendorName;
@@ -26,51 +26,61 @@ public class Builder implements Serializable {
 		this.builderAddress = builderAddress;
 		this.builderPhoneNumber = builderPhoneNumber;
 	}
+
 	public String getVendorName() {
 		return vendorName;
 	}
+
 	public void setVendorName(String vendorName) {
 		this.vendorName = vendorName;
 	}
+
 	public String getVendorAddress() {
 		return vendorAddress;
 	}
+
 	public void setVendorAddress(String vendorAddress) {
 		this.vendorAddress = vendorAddress;
 	}
+
 	public long getVendorRefNum() {
 		return vendorRefNum;
 	}
+
 	public void setVendorRefNum(long vendorRefNum) {
 		this.vendorRefNum = vendorRefNum;
 	}
+
 	public String getBuilderName() {
 		return builderName;
 	}
+
 	public void setBuilderName(String builderName) {
 		this.builderName = builderName;
 	}
+
 	public long getBuilderRefNum() {
 		return builderRefNum;
 	}
+
 	public void setBuilderRefNum(long builderRefNum) {
 		this.builderRefNum = builderRefNum;
 	}
+
 	public String getBuilderAddress() {
 		return builderAddress;
 	}
+
 	public void setBuilderAddress(String builderAddress) {
 		this.builderAddress = builderAddress;
 	}
+
 	public int getBuilderPhoneNumber() {
 		return builderPhoneNumber;
 	}
+
 	public void setBuilderPhoneNumber(int builderPhoneNumber) {
 		this.builderPhoneNumber = builderPhoneNumber;
 	}
-	
-	
-	
-	
 
 }
--- src/ca/sheridancollege/beans/ConstructionPersonnel.java
+++ src/ca/sheridancollege/beans/ConstructionPersonnel.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 
 @Entity
 public class ConstructionPersonnel implements Serializable {
-	
+
 	@Id
 	@GeneratedValue
 	private int id;
@@ -16,11 +16,11 @@ public class ConstructionPersonnel implements Serializable {
 	private String trade;
 	private long phoneNumber;
 	private String email;
-	
-	public ConstructionPersonnel(){
-		
+
+	public ConstructionPersonnel() {
+
 	}
-	
+
 	public ConstructionPersonnel(int id, String name, String trade, long phoneNumber, String email) {
 		this.id = id;
 		this.name = name;
@@ -28,40 +28,49 @@ public class ConstructionPersonnel implements Serializable {
 		this.phoneNumber = phoneNumber;
 		this.email = email;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getTrade() {
 		return trade;
 	}
+
 	public void setTrade(String trade) {
 		this.trade = trade;
 	}
+
 	public long getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	public void getWorkOrder(int constructionPersonnelId){
-		//needs logic
+
+	public void getWorkOrder(int constructionPersonnelId) {
+		// needs logic
 	}
 
 }
