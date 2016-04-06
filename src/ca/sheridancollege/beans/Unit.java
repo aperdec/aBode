7ae46ab70f87faad30715db07c7c6f1a5ea54aa3
catpoehlman <cat.poehlman@gmail.com>
2016-04-06 11:50:57 -0400

    Unit bean created method needs logic addDeficiency()

---
+++ src/ca/sheridancollege/beans/Unit.java
@@ -0,0 +1,86 @@
+package ca.sheridancollege.beans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Unit implements Serializable {
+
+	@Id
+	private long homeEnrollmentNumber;
+	private int lotNumber;
+	private String address;
+	private HomeOwner homeOwner;
+	private String projectName;
+	private ArrayList<Deficiency> deficiencies;
+	
+	public Unit(){
+		
+	}
+
+	public Unit(long homeEnrollmentNumber, int lotNumber, String address, HomeOwner homeOwner, String projectName,
+			ArrayList<Deficiency> deficiencies) {
+		this.homeEnrollmentNumber = homeEnrollmentNumber;
+		this.lotNumber = lotNumber;
+		this.address = address;
+		this.homeOwner = homeOwner;
+		this.projectName = projectName;
+		this.deficiencies = deficiencies;
+	}
+
+	public long getHomeEnrollmentNumber() {
+		return homeEnrollmentNumber;
+	}
+
+	public void setHomeEnrollmentNumber(long homeEnrollmentNumber) {
+		this.homeEnrollmentNumber = homeEnrollmentNumber;
+	}
+
+	public int getLotNumber() {
+		return lotNumber;
+	}
+
+	public void setLotNumber(int lotNumber) {
+		this.lotNumber = lotNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public HomeOwner getHomeOwner() {
+		return homeOwner;
+	}
+
+	public void setHomeOwner(HomeOwner homeOwner) {
+		this.homeOwner = homeOwner;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public ArrayList<Deficiency> getDeficiencies() {
+		return deficiencies;
+	}
+
+	public void setDeficiencies(ArrayList<Deficiency> deficiencies) {
+		this.deficiencies = deficiencies;
+	}
+	
+	public void addDeficiency(int deficiencyId){
+		//needs logic
+	}
+
+}
