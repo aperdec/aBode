5d60ad4f8fcf7a74ba35348356ccf40218fe6288
perds <aperdec@gmail.com>
2016-04-07 14:10:14 -0400

    removed dao unused methods for subject

--- src/ca/sheridancollege/dao/DAO.java
+++ src/ca/sheridancollege/dao/DAO.java
@@ -2,75 +2,16 @@ package ca.sheridancollege.dao;
 
 import java.util.List;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import ca.sheridancollege.beans.Subject;
 import ca.sheridancollege.beans.User;
 
 public class DAO {
 
 	SessionFactory sessionFactory = new Configuration().configure("ca/sheridancollege/config/hibernate.cfg.xml")
 			.buildSessionFactory();
-
-	public void saveSubject(Subject subject) {
-
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
-		session.saveOrUpdate(subject);
-
-		session.getTransaction().commit();
-		session.close();
-
-	}
-
-	public List<Subject> getSubjectList() {
-
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
-		Query query = session.createQuery("from Subject");
-		List<Subject> subjectList = (List<Subject>) query.list();
-
-		session.getTransaction().commit();
-		session.close();
-
-		return subjectList;
-
-	}
-
-	public List<Subject> getSubject(String title) {
-
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
-		Query query = session.getNamedQuery("Subject.byTitle");
-
-		query.setString("title", title);
-
-		List<Subject> subjectList = (List<Subject>) query.list();
-
-		session.getTransaction().commit();
-		session.close();
-
-		return subjectList;
-
-	}
-
-	public void removeSubject(String title) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
-		List<Subject> subjectToDelete = getSubject(title);
-		session.delete(subjectToDelete.get(0));
-
-		session.getTransaction().commit();
-		session.close();
-
-	}
 
 	public User findByUserName(String username) {
 		List<User> users = sessionFactory.openSession().createQuery("from User where username=:user")
