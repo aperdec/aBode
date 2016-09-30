174fb7470adedc194cb5bfb6ee252a2567987b8c
perds <aperdec@gmail.com>
2016-09-30 14:40:18 -0400

    moved home controller data to service class

--- src/ca/sheridancollege/controllers/HomeController.java
+++ src/ca/sheridancollege/controllers/HomeController.java
@@ -204,20 +204,6 @@ public class HomeController {
     @RequestMapping("/displayBuildingProjects")
     public String displayBuildingProjects(Model model) {
 
-        List<String> projectList = new ArrayList<>();
-
-
-        projectList.add("Blue Skys Project"); // Fake project
-
-        //Condominiums by Times Group Inc
-        projectList.add("Eden Park II");
-        projectList.add("River Park");
-        projectList.add("River Walk East");
-        //coming soon
-        projectList.add("Luxr Towns");
-        projectList.add("Village Park Phase II");
-        projectList.add("River Side");
-        model.addAttribute("projectList", projectList);
         model = controllerServices.displayBuildingProjects(model);
 
         return "displayBuildingProjects";
