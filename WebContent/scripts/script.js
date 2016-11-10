a589a142f231f742aaae10c72eb49d92f05fceea
perds <aperdec@gmail.com>
2016-11-10 15:24:33 -0500

    fixed the merge

--- WebContent/scripts/script.js
+++ WebContent/scripts/script.js
@@ -3,18 +3,37 @@ var e;
 var used;
 function hide(){
 used = document.getElementById("repName").value;
-document.getElementById("nextBtn").style.visibility = 'hidden';
-document.getElementById("saveBtn").style.visibility = 'hidden';
-document.getElementById("nextBtn").style.visibility = 'visible';
+//disable save on load
+$( "#saveBtn" ).prop( "disabled", true );
+
+//test not hidden
+
+$( "#nextBtn" ).prop( "disabled", false );
+$( "#nextBtn" ).prop( "disabled", true );
+
+//document.getElementById("nextBtn").style.visibility = 'visible';
+//document.getElementById("saveBtn").style.visibility = 'visible';
 
 if (used === ""){
 	//alert(used);
-	document.getElementById("nextBtn").style.visibility = 'hidden';
+	document.getElementById("nextBtn").style.visibility = 'visible';
 }
 }
 
 function showSave(){
+	$( "#saveBtn" ).prop( "disabled", false );
+	//$( "#nextBtn" ).prop( "disabled", false );
+//	$('a').disable(false);
+
 	document.getElementById("saveBtn").style.visibility = 'visible';
+}
+
+function enableNextBtn(){
+	$('a').disable(false);
+}
+
+function disableNextBtn(){
+	$('a').disable(false);
 }
 
 function acceptSig(){
@@ -27,11 +46,12 @@ function acceptSig(){
 function ValidateHEN() {
 	e = document.getElementById("homeEnrollmentNumber").value;
     //if you need text to be compared then use
-    if(e <= 0 || e ==null || e=="") {
-        $('#alert').show();
-        $('#selectHEN').show();
-        return false;
-    } else if (isNaN(e)){
+//    if(e <= 0 || e ==null || e=="") {
+//        $('#alert').show();
+//        $('#selectHEN').show();
+//        return false;
+//    } else
+    if (isNaN(e)){
     	$('#alert').show();
         $('#selectHEN').show();
         return false;
@@ -50,6 +70,28 @@ function Validate() {
         return false;
     }
 }
+
+//Function to enable disable Next button HREF
+$(function() {
+    jQuery.fn.extend({
+        disable: function(state) {
+            return this.each(function() {
+                var $this = $(this);
+                if($this.is('input, button'))
+                    this.disabled = state;
+                else
+                    $this.toggleClass('disabled', state);
+            });
+        }
+    });
+
+    $('a').disable(true);
+
+    $('body').on('click', 'a.disabled', function(event) {
+        event.preventDefault();
+    });
+});
+
 
 function verify() {
 	varpassword1 = document.forms['form']['password'].value;
